5e18cc64959a034bc43e15e01eba88ab2a0465aa
Basykov <dadadavir@gmail.com>
2024-03-09 18:55:24 +0200

    Binary tree

---
+++ homework_bin.py
@@ -0,0 +1,91 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, root, key):
+        if root is None:
+            return Node(key)
+        if key < root.key:
+            root.left = self._insert_recursive(root.left, key)
+        else:
+            root.right = self._insert_recursive(root.right, key)
+        return root
+
+    def delete(self, key):
+        self.root = self._delete_recursive(self.root, key)
+
+    def _delete_recursive(self, root, key):
+        if root is None:
+            return root
+        if key < root.key:
+            root.left = self._delete_recursive(root.left, key)
+        elif key > root.key:
+            root.right = self._delete_recursive(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            temp = self._min_value_node(root.right)
+            root.key = temp.key
+            root.right = self._delete_recursive(root.right, temp.key)
+        return root
+
+    def _min_value_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder_traversal_recursive(self.root, result)
+        return result
+
+    def _inorder_traversal_recursive(self, root, result):
+        if root:
+            self._inorder_traversal_recursive(root.left, result)
+            result.append(root.key)
+            self._inorder_traversal_recursive(root.right, result)
+
+
+tree = BinarySearchTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(15)
+tree.insert(3)
+tree.insert(7)
+tree.insert(12)
+tree.insert(18)
+
+print("Дерево після вставки:")
+print(tree.inorder_traversal())
+
+#      10
+#    /    \ 
+#   5      15
+#  / \    /  \
+# 3  7  12   18
+
+tree.delete(10)
+
+print("Дерево після видалення елемента з ключем 10:")
+print(tree.inorder_traversal())
+
+#      12
+#    /    \ 
+#   5      15
+#  / \    /  \
+# 3  7       18
+
+
+
