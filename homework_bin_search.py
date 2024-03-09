c22d4d78851618b3f0e319ce0dcc7500cd5a9fc8
Basykov <dadadavir@gmail.com>
2024-03-09 19:35:29 +0200

    Bin search

---
+++ homework_bin_search.py
@@ -0,0 +1,27 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (right + left) // 2
+        print(mid)
+        print(arr[mid])
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+        
+
+    return -1  
+
+
+arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+target = 12
+result = binary_search(arr, target)
+if result != -1:
+    print(f"{target} десь на {result} місці.")
+else:
+    print("Нема =(")
+
